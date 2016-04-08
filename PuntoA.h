de1bbd4bbc541203d28d95f89b7cd98838336b17
joakoracing <joakoracing@hotmail.com>
2016-04-08 14:43:20 -0300

    Update PuntoA.h

--- PuntoA.h
+++ PuntoA.h
@@ -1,3 +1,10 @@
-#include "PUNTOA.c"
+#ifndef __PUNTOA_H__
+    #define __PUNTOA_H__
 
-void AsignarValores (float *Landa, float *m1, float *m2, float *e, float *a, float *h, float *k);
+    #define N 36
+
+    float Elevar(float a, float b);
+
+    void AsignarValores (float *Landa, float *m1, float *m2, float *e, float *a, float *h, float *k, float *m, float *g);
+
+#endif // __PUNTOA_H__
