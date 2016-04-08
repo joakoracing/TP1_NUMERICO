63b4147e8690080d48b34379a5e05fbc835c8cb8
joakoracing <joakoracing@hotmail.com>
2016-04-08 14:42:12 -0300

    Creo q ya esta

--- AlgoritmoB.c
+++ AlgoritmoB.c
@@ -5,16 +5,20 @@
 
 int main (int argc, char* argv[])
 {
-    float Landa, m1, m2, e, a, h, k, uo, vo;
-    int n;
+    float Landa, m1, m2, e, a, h2, k, m, g, uo, vo;
+    int n, u[N-1], v[N-1];
 
-    AsignarValores(&Landa, &m1, &m2, &e, &a, &h, &k);
+    AsignarValores(&Landa, &m1, &m2, &e, &a, &h2, &k, &m, &g);
     uo = 1 / ( a * ( 1 - e ) );
+    u[0] = uo;
     vo = 0;
+    v[0] = vo;
     for (n=0; n<N; n++)
     {
-
+        u[n+1] = u[n] + (k * v[n]);
+        v[n+1] = v[n] - (k * u[n]) + ((k * g * m) / h2);
     }
     n++;
     return RES_OK;
 }
+
