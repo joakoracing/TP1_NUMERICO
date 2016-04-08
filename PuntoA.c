4cec214318bd758b434489c67cd4cc6b01dca890
joakoracing <joakoracing@hotmail.com>
2016-04-08 14:42:49 -0300

    Creo q ya esta

--- PuntoA.c
+++ PuntoA.c
@@ -1,39 +1,31 @@
-/*Landa = 97537/(900000)*/
-#define N 360
-#define M
-#define G
+/*Landa = 97537/(90000)*/
+#include "PUNTOA.h"
 
-float Elevar (int a, int b)
+
+float Elevar(float a, float b)
 {
-    float res;
     int i;
-    res = 1;
-    if (b=0)
-        return 1;
-    for (i=1; i<=b; i++)
-    {
-        res = res*a;
-    }
-    return (res);
+    float res=1;
+    if (b==0)
+        res=1;
+    if (b>0)
+        for(i=1; i<=b; i++)
+            {res=(res*a);}
+    if (b<0)
+        for(i=1; i<=b; i++)
+            {res=(res*(1/a));}
+    return res;
 }
 
-void AsignarValores (float *Landa, float *m1, float *m2, float *e, float *a, float *h, float *k)  /*multiplicar todo por 10*/
+void AsignarValores (float *Landa, float *m1, float *m2, float *e, float *a, float *h2, float *k, float *m, float *g)
 {
-    float h2;
-
-    *Landa = 0.1083744444444444;
-
-    *m1 = (0,2155676074444444 * Elevar (10,30)); /*kg*/
-
-    *m2 = (0,3577440411111111 * Elevar (10,30)); /*kg*/
-
-    *e = 1.897126219;
-
-    *a = (0.06801541203 * Elevar (10,7)); /*kg*/
-
-    h2 = (*a) * G * M * (1 - ((*e) * (*e))); /*m^4/s^2*/
-
-    *h = sqrt (h2);
-
+    *Landa = 1.083744444444444;
+    *g = 6.673 * Elevar(10,-11);
+    *m1 = (2.155676074444444 * Elevar(10,30)); /*kg*/
+    *m2 = (3.577440411111111 * Elevar(10,23)); /*kg*/
+    *m = *m1 + *m2;
+    *e = 0.111894539517446;
+    *a = (6.801541203 * Elevar(10,7)); /*kg*/
+    *h2 = (*a) * (*g) * (*m) * (1 - Elevar((*e),2)); /*m^4/s^2*/
     *k = ( 2 * 3.14159265359 ) / N;
 }
